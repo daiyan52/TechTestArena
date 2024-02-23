295bb97b3ac0ec114cb23d20274478d9c1663617
daiyan52 <rehanalamkkr@gmail.com>
2024-02-24 01:22:43 +0530

    finally done

--- Exam_Portal/quiz/models.py
+++ Exam_Portal/quiz/models.py
@@ -1,3 +1,37 @@
+# from django.db import models
+# from django.contrib.auth.models import User
+
+# # Create your models here.
+# class Quiz(models.Model):
+#     title = models.CharField(max_length=255)
+#     description = models.TextField()
+
+#     def __str__(self):
+#         return self.title
+
+# class Question(models.Model):
+#     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+#     text = models.TextField()
+
+#     def __str__(self):
+#         return self.text
+
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     text = models.CharField(max_length=255)
+#     is_correct = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         return self.text
+
+# class UserResponse(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return f"{self.user.username}'s response to '{self.question.text}'"
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -12,6 +46,10 @@ class Quiz(models.Model):
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     text = models.TextField()
+    choice1 = models.ForeignKey('Choice', on_delete=models.CASCADE, related_name='choice1', null=True, blank=True)
+    choice2 = models.ForeignKey('Choice', on_delete=models.CASCADE, related_name='choice2', null=True, blank=True)
+    choice3 = models.ForeignKey('Choice', on_delete=models.CASCADE, related_name='choice3', null=True, blank=True)
+    choice4 = models.ForeignKey('Choice', on_delete=models.CASCADE, related_name='choice4', null=True, blank=True)
 
     def __str__(self):
         return self.text
--- Exam_Portal/quiz/urls.py
+++ Exam_Portal/quiz/urls.py
@@ -4,7 +4,7 @@ from quiz import views
 app_name = 'quiz'
 
 urlpatterns = [
-    path('/', views.quiz_list, name='quiz_list'),
+    path('', views.quiz_list, name='quiz_list'),
     path('quiz_list1/', views.quiz_list1, name='quiz_list1'),
     path('quiz_list2/', views.quiz_list2, name='quiz_list2'),
     path('quiz/<int:quiz_id>/', views.take_quiz, name='take_quiz'),
