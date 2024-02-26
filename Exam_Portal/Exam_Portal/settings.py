33620d21666cf4f527d97799038110b91c83dcbc
daiyan52 <rehanalamkkr@gmail.com>
2024-02-26 13:38:34 +0530

    last commit

--- Exam_Portal/Exam_Portal/settings.py
+++ Exam_Portal/Exam_Portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zw+(kc#-nym9@&yltlxckr=@=c2@xvkfmpjiw00pslwxz@9oy7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
